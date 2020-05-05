7782e1ae9429a4ffb872a5e205cb7b54eefbae41
Jonny Chang <58116035+jonny6015@users.noreply.github.com>
2020-05-06 01:03:26 +0800

    测试相册

--- src/main/java/com/yiyjm/nest/controller/AblumsController.java
+++ src/main/java/com/yiyjm/nest/controller/AblumsController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2020/05/06
  */
 @Controller
-@RequestMapping("/ablums")
 public class AblumsController {
-	@RequestMapping({"/3d/index.html", "ablums/3d/index", "ablums/3d"})
+	@RequestMapping("/hello")
 	public String threePhoto() {
-		return "ablums/3d/index";
+		return "pages/hello";
 	}
 }
